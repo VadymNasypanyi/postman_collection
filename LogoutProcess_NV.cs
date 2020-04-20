779a86e5b49f5f97d49abf690ca5a2d99c79af1b
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2020-04-20 17:00:37 +0300

    Test

---
+++ LogoutProcess_NV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+
+namespace VadimFramework.Engine
+{
+    class LogoutProcess
+    {
+        [FindsBy(How = How.XPath, Using = "//details[@class='details-overlay details-reset js-feature-preview-indicator-container']//summary[@class='Header-link']")]
+        public IWebElement clickHeaderButton { get; set; }
+        [FindsBy(How = How.XPath, Using = "//button[@class='dropdown-item dropdown-signout']")]
+        public IWebElement clickLogoutButton { get; set; }
+    }
+}
---
+++ RepositoryCreator_NV.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+
+namespace VadimFramework.Engine
+{
+    class RepositoryCreator
+    {
+        [FindsBy(How = How.XPath, Using = "//a[@class='btn shelf-cta mx-2 mb-3']")]
+        public IWebElement startProjectButton { get; set; }
+        [FindsBy(How = How.XPath, Using = "//input[@id='repository_name']")]
+        public IWebElement typeRepositoryName { get; set; }
+        [FindsBy(How = How.XPath, Using = "//input[@id='repository_description']")]
+        public IWebElement typeDescription { get; set; }
+        [FindsBy(How = How.XPath, Using = "//input[@id='repository_auto_init']")]
+        public IWebElement readMeRadioButton { get; set; }
+        [FindsBy(How = How.XPath, Using = "//button[@class='btn btn-primary first-in-line']")]
+        public IWebElement createRepositoryButton { get; set; }
+    }
+}
